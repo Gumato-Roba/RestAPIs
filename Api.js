20cbb6516967ffe0b63873be0f21228100a5ccc5
Gumato-Roba <gumatorobba@gmail.com>
2022-08-23 15:40:08 +0300

    changes

--- Api.js
+++ Api.js
@@ -1,28 +1,60 @@
 
- //script.js
+//script.js
 const products = document.getElementById("products");
 const fruList = document.getElementById("fruList");
 const vegList = document.getElementById("vegList");
+const prodHeader = document.getElementById("prodHeader");
+const fruitHeader = document.getElementById("fruitHeader");
+const vegHeader = document.getElementById("vegHeader");
 
 //Fetch Item
-async function getdata(item,element){
+async function getdata(item){
   const response = await fetch("http://localhost:5000/"+item)
   .then((response)=>{
    return response.json();
    }).then((data)=>{
     data.forEach((product) => {
      const li = document.createElement("li");
-     li.innerHTML = product["name"].concat("<br/>").concat(product["category"]);
-     element.appendChild(li);
+     if (item != "products") {
+      li.innerHTML = product["name"];
+      if (item =='products/fruits') {
+       fruList.style.display='block';
+       fruitHeader.style.display='block';
+       fruList.appendChild(li);
+      }else{
+       vegHeader.style.display='block';
+       vegList.style.display='block';
+       vegList.appendChild(li);
+      }
+     }else{
+      li.innerHTML = product["name"].concat("<br/>").concat(product["category"]);
+      prodHeader.style.display='block';
+      products.style.display='block';
+      products.appendChild(li);
+     }
+     
    });
  })
 }
 
-//Products
-getdata("products",products);
+const queryString = window.location.search;
+//get the query part
+const query = queryString.split('?')[1];
 
-//fruits
-getdata("products/fruits",fruList);
+//hide tha page headers
+vegList.style.display='none';
+fruList.style.display='none';
+products.style.display='none';
+prodHeader.style.display='none';
+fruitHeader.style.display='none';
+vegHeader.style.display='none';
 
-//vegetables
-getdata("products/vegetables",vegList);
+
+//Display result
+console.log(query);
+getdata(query);
+
+
+
+
+ 
